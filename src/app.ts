273758b136485b9521cf85d341c0d4544c88658c
JeongJaeah <apmabi@mrdevello.com>
2024-05-21 18:14:44 +0900

    no message

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,7 @@ async function startServer(app: core.Express) {
 
   // start the server
   server = app.listen(8080, () => {
-    console.log('ver7')
+    console.log('ver8')
     if (process?.send) {
       process.send('ready');
     }
